010b5aeb1abc36ca50c8ef6fe4d9a3ada5a78108
garlickknight <garlickknight@gmail.com>
2025-03-08 11:45:34 -0700

    Finished exercise 5!

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,23 +1,49 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
-        Console.Write("What is your Name?");
-        string name = Console.ReadLine();
-        Console.Write("What is your favorite Number?");
-        string numberPrompt = Console.ReadLine();
-        int favoriteNumber = int.Parse(numberPrompt);
-        Console.WriteLine($"{name}, your favorite number squared is {SquareNumber(favoriteNumber)}.");
+        static string welcome()
+        {
+            string welcomeMessage = "Welcome to this assingment";
+            return welcomeMessage;
+        }
+        static string name()
+        {
+            Console.Write("What is your name?");
+            string namePrompt = Console.ReadLine();
+            return namePrompt;
+        }
+        static int favoriteNumber()
+        {
+            Console.Write("What is your favorite number?");
+            string numberPrompt = Console.ReadLine();
+            int number = int.Parse(numberPrompt);
+            return number;
+        }
 
         static int SquareNumber(int favoriteNumber)
         {
             int total = favoriteNumber * favoriteNumber;
             return total;
         }
+        static void Display(string welcome)
+        {
+            Console.WriteLine(welcome);
+            Console.WriteLine($"{name()}, your number squared is {SquareNumber(favoriteNumber())}.");
+
+        }
+        // string userName = name();
+        // int userNumber = favoriteNumber();
+        // int numSquared = SquareNumber(userNumber);
+        Display(welcome());
+
     }
 }
