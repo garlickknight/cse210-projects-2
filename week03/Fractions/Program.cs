bec2a831a9d2c661e82672a3e7ebce2396d81773
garlickknight <garlickknight@gmail.com>
2025-09-16 17:44:25 -0600

    made the actiivity 1

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -6,4 +9,26 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
     }
+}
+public class Fractions
+{
+    private int _Top = 0;
+    private int _Bottom = 0;
+
+    public Fractions()
+    {
+        _Top = 1;
+        _Bottom = 1;
+        string fraction = $"{_Top}/{_Bottom}";
+    }
+    public Fractions(int _Top)
+    {
+        _Bottom = 1;
+        string fraction = $"{_Top}/{_Bottom}";
+    }
+    public Fractions(int _Top, int _Bottom)
+    {
+        string fraction = $"{_Top}/{_Bottom}";
+    }
+
 }
